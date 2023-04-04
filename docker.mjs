bdc2151f9f762ba68a785c9f1f815f9e0539b399
Avior Peretz <avior.peretz@aquasec.com>
2023-04-04 22:50:49 +0300

    🎉 chore(ci): add CI scripts and configuration files This commit adds several scripts and configuration files to support continuous integration. The `ci.mjs` script checks for required environment variables and throws an error if any are missing. The `constants.mjs` file exports an array of required environment variables. The `docker.mjs` script logs in to Docker, builds an image, and pushes it to a registry. The `healthcheck.mjs` script sends a GET request to the `/health` endpoint of the application and logs the response. The `sandbox.mjs` script starts a Docker Compose stack for the application. The `swagger.mjs` script generates a Swagger documentation file. The `utils.mjs` file exports a function to get the root path of the project.

---
+++ docker.mjs
@@ -0,0 +1,12 @@
+#!/usr/bin/env zx
+
+console.log("Docker script started");
+try {
+  await $`docker login -u ${process.env.username} -p ${process.env.password}`;
+  await $`docker build . -t ${process.env.image}`;
+  await $`docker push ${process.env.image}`;
+} catch (error) {
+  console.error(error);
+}
+
+console.log("Docker script finished");
